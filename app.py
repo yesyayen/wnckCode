9a47d7f8c2cac16761f4056730f8030b3a692a94
AnanD <yesyayen@gmail.com>
2013-11-12 02:00:38 +0530

    modified

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ from piWindow import PIWindow
 if __name__ == '__main__':
     manager = ScreenManager()
     window = PIWindow(manager.get_active_window())
-    #print window.set_position(0,0)
+    print window.set_position(0,0)
     #print manager.get_screen_size()
     #window.set_geometry(5, 15, 1900, 24, 500, 500)
     #window.maximize()
     #print type(window.get_window().get_workspace())
 
-    print dir(wnck.Screen)
+    #print dir(wnck.Screen)
+    
+    print manager.get_screen_size()
+    (usable_width,usable_height,xorigin, yorigin)=manager.get_usable_size()
+    print (usable_width,usable_height,xorigin, yorigin)
+    
+    
+    
