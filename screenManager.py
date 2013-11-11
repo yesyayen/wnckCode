d8bccd7f64f72b9f34b5075569fb77fe6812daf6
AnanD <yesyayen@gmail.com>
2013-11-12 01:59:37 +0530

    modified

--- screenManager.py
+++ screenManager.py
@@ -8,7 +8,7 @@ class ScreenManager:
         self.screen = wnck.screen_get_default()
         self.screen_width = self.screen.get_width()
         self.screen_height = self.screen.get_height()
-
+    	
     def get_screen_size(self):
         return (self.screen_width, self.screen_height)
 
@@ -17,3 +17,19 @@ class ScreenManager:
         while gtk.events_pending():
             gtk.main_iteration()		
         return self.screen.get_active_window()
+        
+    def get_usable_size(self):
+	    windowStack = self.screen.get_windows_stacked()
+	    for i in range(0,len(windowStack)):
+		if (windowStack[i].get_name() == "unity-launcher"):
+		        (xPos1,yPos1,wid1,hei1)=windowStack[i].get_geometry()
+	        if (windowStack[i].get_name() == "unity-panel"):
+		        (xPos2,yPos2,wid2,hei2)=windowStack[i].get_geometry()
+		
+	    useWidth = self.screen.get_width()-wid1
+       	    useHeight = self.screen.get_height()-hei2
+	    #print windowStack[i].get_geometry()," - ",windowStack[i].get_name()," - ",windowStack[i].get_window_type()
+ 	    #print 'Total screen size ', self.screen.get_width(),self.screen.get_height()
+	    #print 'Usable screen size',useWidth ,useHeight
+	    #print 'Origin is',wid1,',',hei2
+	    return (useWidth,useHeight,wid1,hei2)
