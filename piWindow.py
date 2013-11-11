86a8b30c325cfe5a00174c8c14074d66591c086c
AnanD <yesyayen@gmail.com>
2013-11-12 01:59:00 +0530

    modified resizeMask and gravity value accordingly and added get_window_size function

--- piWindow.py
+++ piWindow.py
@@ -3,19 +3,23 @@ import gtk
 import time
 
 class PIWindow():
-    """Cotroller to abstract out window functions"""
+    """Controller to abstract out window functions"""
 
-    _gravity = 5
-    _resizeMask = 15
+    _gravity =0
+    _resizeMask = 255
     
     def __init__(self, window):
         self._window = window
+       
+    def get_window_size(self):
+        (xPos, yPos, curLength, curHeight) = self._window.get_geometry()
+        return (xPos, yPos, curLength, curHeight)
 
     def set_position(self, xPos, yPos):
         (xCur, yCur, length, height) = self._window.get_geometry()
         self._window.set_geometry(PIWindow._gravity, PIWindow._resizeMask, xPos, yPos, length, height)
 
-    def set_size(self, length, heigth):
+    def set_size(self, length, height):
         (xPos, yPos, curLength, curHeight) = self._window.get_geometry()
         self._window.set_geometry(PIWindow._gravity, PIWindow._resizeMask, xPos, yPos, length, height)
 
@@ -29,6 +33,11 @@ class PIWindow():
     def unmaximize(self):
         if self._window.is_maximized():
             self._window.unmaximize()
+            
+    def minimize(self):
+        self._window.minimize()
 
     def get_window(self):
         return self._window
+    
+    
